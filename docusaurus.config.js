3113d99cfd4b7153b2e6ff4ac7444d6d4cfb1702
steven <coldplay126@gmail.com>
2025-05-11 22:37:06 +0900

    Fix baseUrl 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://laravel.chanhyung.kim',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/laravel-docs-web/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
