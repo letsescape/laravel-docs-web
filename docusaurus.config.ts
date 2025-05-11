b8220d67de0b88461b260ba484fedc5a369be7b5
steven <coldplay126@gmail.com>
2025-05-12 00:40:32 +0900

    Fix typo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://letsescape.github.io',
+  url: 'https://laravel.chanhyung.kim',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/laravel-docs-web/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
