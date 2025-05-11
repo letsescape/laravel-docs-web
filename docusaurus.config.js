e31d0a254fdc4ec1122ee3b2eafc33bfaa4de0db
steven <coldplay126@gmail.com>
2025-05-11 22:32:30 +0900

    Feat 배포 액션 설정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://laravel.chanhyung.kim',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/laravel-docs-web/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
